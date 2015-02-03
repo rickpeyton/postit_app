6eebae6d84ef1bcfc381c385fc303cd06294e90e
Rick Peyton <peytorb@gmail.com>
2015-02-03 06:43:23 -0600

    working through posts controller and forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,48 @@
 class PostsController < ApplicationController
-  def index
+  before_action :set_post, only: [:show, :edit, :update]
 
+  def index
+    @posts = Post.all
   end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Your post was created"
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    binding.pry
+    if @post.save
+      flash[:notice] = "Your post was edited"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:url, :title, :description, :creator)
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
+  has_many :post_categories
+  has_many :categories, through: :post_categories
+
+  validates :title, :url, :description, presence: true
 end
